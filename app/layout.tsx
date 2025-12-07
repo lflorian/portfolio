cee7f1e8d668509a25b1a9e689ab49c31a194e8e
Florian <106911630+lflorian@users.noreply.github.com>
2025-12-07 14:17:01 +0100

    analytics fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 
 import { Header } from "./sections/Header";
@@ -29,6 +30,7 @@ export default function RootLayout({
         <Header />
         <main className="bg-white">
           {children}
+          <Analytics />
         </main>
         <Footer />
       </body>
